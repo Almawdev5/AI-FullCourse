ab4f6e343b7602cb4499264ae46f9f4203ea4c97
Tadai <almawtadele0@gmail.com>
2026-07-17 16:21:03 +0300

    Complete NumPy Lesson 1

---
+++ Numpy/lesson1.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+# Python list
+python_list = [1, 2, 3, 4, 5]
+
+print("Python List:")
+print(python_list)
+
+print("Python List * 2:")
+print(python_list * 2)
+
+print()
+
+# NumPy array
+numpy_array = np.array([1, 2, 3, 4, 5])
+
+print("NumPy Array:")
+print(numpy_array)
+
+print("NumPy Array * 2:")
+print(numpy_array * 2)
+
+print("NumPy Array + 10:")
+print(numpy_array + 10)
+
+print("NumPy Array - 1:")
+print(numpy_array - 1)
+
+print("NumPy Array / 2:")
+print(numpy_array / 2)
